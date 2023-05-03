110533ab949d822bcae8fdeb28e765bd9e8025ea
hasanisokay <hasanisokay@gmail.com>
2023-05-04 01:52:37 +0600

    added photo to error page

--- src/components/errorpage/ErrorPage.jsx
+++ src/components/errorpage/ErrorPage.jsx
@@ -1,16 +1,15 @@
 import { FaceFrownIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
-
+import errorImage from "../../../public/error404.webp"
 const ErrorPage = () => {
     const {error, status} = useRouteError()
     return (
-        <div className='text-center mt-16 overflow-hidden'>
-            <FaceFrownIcon className='m-auto w-40 h-50 text-slate-700'></FaceFrownIcon>
-            <h1 className='text-red-600 text-6xl font-bold'> ERRORRR....</h1>
-            <h1 className='text-red-600 text-8xl font-bold'> {status}</h1>
-            <h1 className='text-red-900 text-2xl font-bold mb-4'> {error ?.message}</h1>
+        <div className='text-center overflow-hidden'>
+            <h1 className='text-red-600 text-xl font-bold'>{status}</h1>
+            <h1 className='text-red-900 text-xl font-bold mb-4'> {error ?.message}</h1>
             <Link to="/" className='btn text-xl font-semibold bg-yellow-400 p-2 rounded'>Back to Home</Link>
+            <div className='lg:w-5/6 mx-auto'><img src={errorImage} className='shadow-xl rounded mt-2 h-96 w-full' alt="error" /></div>
         </div>
     );
 };
--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -23,7 +23,7 @@ const Blog = () => {
           Express is a small framework that sits on top of Node.js's web server functionality to simplify its APIs and add helpful new features. It makes it easier to organize application's functionality with middle ware and routing. Express.js is a framework based on Node.js which is used for building web-application using approaches and principles of Node.js event-driven architecture.
         </p>
       </div>
-      <div>
+      <div className="mb-6">
         <h1 className='text-xl font-semibold mb-2'>Custom hook and its necessity</h1>
         <p>A custom Hook is a JavaScript function whose name starts with <code>use</code> and that may call other Hooks. If you have code in a component that you feel would make sense to extract, either for reuse elsewhere or to keep the component simpler, you can pull that out into a function. </p>
       </div>
