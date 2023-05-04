7e41d6484418f542a2f440aca2922210c87a16c7
hasanisokay <hasanisokay@gmail.com>
2023-05-04 21:23:19 +0600

    added btn to banner and edited margin

--- src/components/pages/homepage-banner/HomeBanner.jsx
+++ src/components/pages/homepage-banner/HomeBanner.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import blurry from "../../../../src/assets/blurry.svg"
+import { Link } from 'react-router-dom';
 
 const HomeBanner = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 lg:gap-0 mt-4 mb-8 gap-2 items-center '> 
-            <div className='h-96 lg:w-4/5'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 md:gap-16 lg:gap-0 lg:mt-4 mt-2 mb-8 gap-4 lg:items-center'> 
+            <div className='lg:h-96 md:h-96  lg:w-4/5 md:w-[75%] mx-auto '>
                 <LazyLoadImage className='h-full w-full rounded'
                 
                 effect='blur' delayTime={6} placeholderSrc={blurry} delayMethod='throttle'
@@ -15,6 +16,7 @@ const HomeBanner = () => {
             <div className='text-center'>
                 <h1 className='text-6xl text-center my-4'>Need <span className='text-red-900'>recipes</span><span className='text-red-600'>?</span> </h1>
                 <p className='text-2xl'>Search for Bangladeshi food recipe among thousands of recipes from professionals!</p>
+                <Link to="/blog"><button className='btn btn-warning'>Explore </button></Link>
             </div>
             
         </div>
